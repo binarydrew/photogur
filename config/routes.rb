35e9e715e40ec723ac11e4e37d13c927de5ee7c5
YO <yo@Steins-MacBook-Pro-3.local>
2013-07-04 14:00:49 -0400

    created adding of pictures by users but not yet savable

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Photogur::Application.routes.draw do
+  post 'pictures' => "pictures#create" 
+
   get 'pictures' => 'pictures#index'
+
+  get 'pictures/new' => 'pictures#new'
 
   get 'pictures/:id' => 'pictures#show', as: "picture"
 end
