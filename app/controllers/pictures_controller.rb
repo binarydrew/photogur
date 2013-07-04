a9f3a2a74561075422240adf667d7eca5ac0e63e
YO <yo@Steins-MacBook-Pro-3.local>
2013-07-04 16:37:21 -0400

    refactored to save new pictures in database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,18 +1,24 @@
 class PicturesController < ApplicationController
 
-	def index
-    @pictures = Pictures.all
-	end
+def index
+   @pictures = Picture.all
+end
 
 def show
    @picture = Picture.find(params[:id])
 end
 
 def new
+  @picture = Picture.new
 end
 
 def create
-	render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+  @picture = Picture.new(params[:picture])
+  if @picture.save
+  	redirect_to pictures_url
+  else
+    render :new
+  end
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Photogur::Application.routes.draw do
+  root to: 'pictures#index'
+
   post 'pictures' => "pictures#create" 
 
   get 'pictures' => 'pictures#index'
 
-  get 'pictures/new' => 'pictures#new'
+  get 'pictures/new' => 'pictures#new' 
 
   get 'pictures/:id' => 'pictures#show', as: "picture"
 end
